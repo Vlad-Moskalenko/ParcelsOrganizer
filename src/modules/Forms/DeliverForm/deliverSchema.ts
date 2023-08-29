5111aa9fd5dc8fd389767280481136109cb836b5
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 07:08:11 +0300

    feat: add only letters validation for location and destination

--- src/modules/Forms/DeliverForm/deliverSchema.ts
+++ src/modules/Forms/DeliverForm/deliverSchema.ts
@@ -1,7 +1,17 @@
 import * as Yup from 'yup';
 
 export const deliverSchema = Yup.object().shape({
-  location: Yup.string().min(1, 'Too Short!').max(20, 'Too Long!').required('Required'),
-  destination: Yup.string().min(1, 'Too Short!').max(20, 'Too Long!').required('Required'),
-  date: Yup.date().min(new Date(new Date().getTime() - 24*60*60*1000), 'Date must be present or in the future'),
+  location: Yup.string()
+    .matches(/^[A-Za-z\s-]+$/, 'Only letters are allowed')
+    .min(1, 'Too Short!')
+    .max(20, 'Too Long!')
+    .required('Required'),
+  destination: Yup.string()
+    .matches(/^[A-Za-z\s-]+$/, 'Only letters are allowed')
+    .min(1, 'Too Short!')
+    .max(20, 'Too Long!')
+    .required('Required'),
+  date: Yup.date()
+    .min(new Date(new Date().getTime() - 24*60*60*1000), 'Date must be in present or in the future')
+    .max(new Date(new Date().getTime() - 365*24*60*60*1000), 'Date should not be older than 1 year from current date'),
 });
--- src/modules/Forms/OrderForm/orderSchema.ts
+++ src/modules/Forms/OrderForm/orderSchema.ts
@@ -1,9 +1,19 @@
 import * as Yup from 'yup';
 
 export const orderSchema = Yup.object().shape({
-  location: Yup.string().min(1, 'Too Short!').max(20, 'Too Long!').required('Required'),
-  destination: Yup.string().min(1, 'Too Short!').max(20, 'Too Long!').required('Required'),
-  date: Yup.date().min(new Date(new Date().getTime() - 24*60*60*1000), 'Date must be present or in the future'),
+  location: Yup.string()
+    .matches(/^[A-Za-z\s-]+$/, 'Only letters are allowed')
+    .min(1, 'Too Short!')
+    .max(20, 'Too Long!')
+    .required('Required'),
+  destination: Yup.string()
+    .matches(/^[A-Za-z\s-]+$/, 'Only letters are allowed')
+    .min(1, 'Too Short!')
+    .max(20, 'Too Long!')
+    .required('Required'),
+  date: Yup.date()
+    .min(new Date(new Date().getTime() - 24*60*60*1000), 'Date must be in present or in the future')
+    .max(new Date(new Date().getTime() - 365*24*60*60*1000), 'Date should not be older than 1 year from current date'),
   type: Yup.string().oneOf(['gadgets', 'drinks', 'clothes', 'medicines', 'other'],'Invalid type'),
   description: Yup.string().min(1, 'Too Short!').max(200, 'Too Long!')
 });
