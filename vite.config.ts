40d6dae2c502f5c92ecc68704ab3d0ce657c2a49
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 14:21:36 +0300

    feat: add gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gotoinc/",
   plugins: [react()],
   resolve: {
     alias: {
       src: '/src'
     }
-  }
+  },
 })
