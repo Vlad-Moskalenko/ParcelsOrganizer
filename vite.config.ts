19f165940d05d04f52061ca0038ae8ff83b71856
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 16:54:55 +0300

    try to fix deploy 5

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/gotoinc",
+  base: '/gotoinc/',
   plugins: [react()],
   resolve: {
     alias: {
