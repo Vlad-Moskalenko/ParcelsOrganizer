35671d6ba5a144b389f029db416bbed2dfebdb0d
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 20:32:47 +0300

    fix: fix sort parcel when date is undefined

--- src/utils/sortParcelsByDate.ts
+++ src/utils/sortParcelsByDate.ts
@@ -3,9 +3,9 @@ import { ParcelState } from "src/entities/ParcelState";
 export const sortParcelsByDate = (parcels: ParcelState[], sortBy: string) => {
   switch (sortBy) {
     case 'asc':
-      return [...parcels].sort((a, b) => Date.parse(a.date || '') - Date.parse(b.date || ''));
+      return [...parcels].sort((a, b) => Date.parse(a.date || '0') - Date.parse(b.date || '0'));
     case 'desc':
-      return [...parcels].sort((a, b) => Date.parse(b.date || '') - Date.parse(a.date || ''));
+      return [...parcels].sort((a, b) => Date.parse(b.date || '0') - Date.parse(a.date || '0'));
     case 'default':
       return [...parcels].sort((a, b) => a.createdAt - b.createdAt);
     default:
