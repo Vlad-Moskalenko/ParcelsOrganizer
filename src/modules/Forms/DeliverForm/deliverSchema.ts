c739ea320badd8acf0d2e1f84213f136fe9696bc
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 06:56:23 +0300

    fix: date validation

--- src/modules/Forms/DeliverForm/deliverSchema.ts
+++ src/modules/Forms/DeliverForm/deliverSchema.ts
@@ -3,5 +3,5 @@ import * as Yup from 'yup';
 export const deliverSchema = Yup.object().shape({
   location: Yup.string().min(1, 'Too Short!').max(20, 'Too Long!').required('Required'),
   destination: Yup.string().min(1, 'Too Short!').max(20, 'Too Long!').required('Required'),
-  date: Yup.date().min(new Date(), 'Date must be present or in the future'),
+  date: Yup.date().min(new Date(new Date().getTime() - 24*60*60*1000), 'Date must be present or in the future'),
 });
