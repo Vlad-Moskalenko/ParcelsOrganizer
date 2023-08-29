c427567a309fa732342dfaa0f8942a754e628cfa
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 17:22:53 +0300

    try to fix deploy 5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,42 +14,45 @@ import RegisterLazyPage from '../pages/RegisterPage/RegisterLazyPage';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import WelcomeLazyPage from 'src/pages/WelcomePage/WelcomeLazyPage';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <WelcomeLazyPage />,
-      },
-      {
-        path: ROUTES.REQUESTS,
-        element: <ParcelsLazyPage />,
-      },
-      {
-        path: ROUTES.CREATE_REQUEST,
-        element: <CreateParcelLazyPage />,
-        children: [
-          {
-            path: ROUTES.CREATE_ORDER,
-            element: <OrderLazyPage />,
-          },
-          {
-            path: ROUTES.CREATE_DELIVER,
-            element: <DeliverLazyPage />,
-          },
-        ],
-      },
-      {
-        path: ROUTES.REGISTER,
-        element: <RestrictedRoute redirectTo={ROUTES.REQUESTS} component={RegisterLazyPage} />,
-      },
-      {
-        path: ROUTES.LOGIN,
-        element: <RestrictedRoute redirectTo={ROUTES.REQUESTS} component={LoginLazyPage} />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <WelcomeLazyPage />,
+        },
+        {
+          path: ROUTES.REQUESTS,
+          element: <ParcelsLazyPage />,
+        },
+        {
+          path: ROUTES.CREATE_REQUEST,
+          element: <CreateParcelLazyPage />,
+          children: [
+            {
+              path: ROUTES.CREATE_ORDER,
+              element: <OrderLazyPage />,
+            },
+            {
+              path: ROUTES.CREATE_DELIVER,
+              element: <DeliverLazyPage />,
+            },
+          ],
+        },
+        {
+          path: ROUTES.REGISTER,
+          element: <RestrictedRoute redirectTo={ROUTES.REQUESTS} component={RegisterLazyPage} />,
+        },
+        {
+          path: ROUTES.LOGIN,
+          element: <RestrictedRoute redirectTo={ROUTES.REQUESTS} component={LoginLazyPage} />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.DEV ? '/' : '/gotoinc/' }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/gotoinc/',
-  plugins: [react()],
-  resolve: {
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    resolve: {
     alias: {
       src: '/src'
     }
   },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/gotoinc/'
+  }
+
+  return config
 })
