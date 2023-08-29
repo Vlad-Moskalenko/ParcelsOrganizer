62cb6d7a10a1aa03d708560d5b66c524f521a8a3
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-29 07:10:10 +0300

    fix: fix max Date for parcels

--- src/modules/Forms/DeliverForm/deliverSchema.ts
+++ src/modules/Forms/DeliverForm/deliverSchema.ts
@@ -13,5 +13,5 @@ export const deliverSchema = Yup.object().shape({
     .required('Required'),
   date: Yup.date()
     .min(new Date(new Date().getTime() - 24*60*60*1000), 'Date must be in present or in the future')
-    .max(new Date(new Date().getTime() - 365*24*60*60*1000), 'Date should not be older than 1 year from current date'),
+    .max(new Date(new Date().getTime() + 365*24*60*60*1000), 'Date should not be older than 1 year from current date'),
 });
